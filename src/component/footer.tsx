3c7814f98aabfd764450183af4cbf5b1b7a33353
bektidev <bekti@primaku.com>
2023-08-31 22:29:03 +0700

    fix global layout

--- src/component/footer.tsx
+++ src/component/footer.tsx
@@ -1,25 +1,81 @@
-import React from "react"
+import React from "react";
 
-export const Footer =()=>{
-    return(
-        <>
-      <div className="container-lg bg-slate-900">
+export const Footer = () => {
+  return (
+    <>
+      <div className="container-lg bg-slate-900 h-auto py-20">
         <div className="container">
-        <div className="grid grid-cols-2 m-8 justify-center items-center z-0">
-                  <div className="p-8">
-                    <p className="font-medium">Title</p>
-                  </div>    
-                  <div className="p-8">
-                    <p className="font-medium">Title</p>
-                  </div>    
-                  <div className="p-8">
-                    <p className="font-medium">Title</p>
-                  </div>    
-        </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 m-8 justify-center z-0">
+            <div className="p-8 col-start-1 col-span-2">
+              <p className="font-bold text-white">
+                CV. Laradesk Teknologi Solusi
+              </p>
+              <br />
+              <p className="text-slate-400">
+                Kami adalah inovator kreatif dalam dunia teknologi dan
+                advertising. Dengan semangat kami yang tak terbatas dalam
+                merangkul perkembangan terkini, kami mempersembahkan
+                solusi-solusi luar biasa untuk membawa bisnismu mencapai
+                kesuksesan
+              </p>
+            </div>
+            <div className="p-8">
+              <p className="font-medium text-white text-bold">Navigation</p>
+              <br />
+              <ul className="list-none">
+                <li>
+                  <a
+                    className="underline decoration-1 text-slate-400"
+                    href="/home"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="underline decoration-1 text-slate-400"
+                    href="/service"
+                  >
+                    Layanan
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="underline decoration-1 text-slate-400"
+                    href="/contact"
+                  >
+                    Kontak
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="underline decoration-1 text-slate-400"
+                    href="/about"
+                  >
+                    Tentang Kami
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="p-8">
+              <p className="font-medium text-white text-bold">Follow Us</p>
+              <br />
+              <ul className="list-none">
+                <li>
+                  <a
+                    className="underline decoration-1 text-slate-400"
+                    href="https://www.instagram.com/laratech.id"
+                  >
+                    <i className="fab fa-instagram"></i> Instagram
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
-    )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import laratech from '../assets/laratech.png'
 
 function Navbar() {
 
@@ -6,7 +7,7 @@ function Navbar() {
 
   const onToggleMenu = (event: React.MouseEvent<HTMLElement>) => {
     toogleIcon == 'fa-solid fa-bars cursor-pointer text-2xl md:hidden text-slate-200' ? 
-    setToogleIcon('fa-solid fa-xmark cursor-pointer text-2xl md:hidden text-slate-200'):
+    setToogleIcon('fa-solid fa-xmark cursor-pointer text-2xl md:hidden text-slate-200 z-20'):
     setToogleIcon('fa-solid fa-bars cursor-pointer text-2xl md:hidden text-slate-200')
     const navlink = document.querySelector('.nav-links')
     navlink?.classList.toggle('top-[3%]')
@@ -16,11 +17,11 @@ function Navbar() {
     <>
       <div className="container-lg bg-slate-900">
       <nav className="flex justify-between items-center w-[80%] mx-auto">
-        <div>
-          <p className="my-4 md:m-8 text-lg font-bold text-slate-200 cursor-pointer">Logos</p>
+        <div className="z-20">
+          <img src={laratech} className="cursor-pointer w-48 my-4 md:m-7" alt="laratech indonesia" />
         </div>
-        <div className="nav-links duration-500 md:static absolute md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 my-5 z-10 bg-slate-900">
-          <ul className="flex md:flex-row flex-col md:items-center md:gap-[3vw] gap-4">
+        <div className="nav-links duration-500 md:static absolute md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 my-11 z-10 bg-slate-900">
+          <ul className="flex md:flex-row flex-col md:items-center md:gap-[3vw] gap-4 mx-8">
             <li>
               <a href="#" className="text-lg font-bold text-slate-200 hover:text-gray-500 hover:border-b-4 border-slate-500 py-2 px-1">Home</a>
             </li>
@@ -36,7 +37,6 @@ function Navbar() {
           </ul>
         </div>
         <div className="flex items-center gap-6 md:hidden">
-          {/* <button className="bg-slate-200 px-5 py-2 rounded-full">Sign In</button> */}
           <i onClick={onToggleMenu} className={toogleIcon}></i>
         </div>
 
