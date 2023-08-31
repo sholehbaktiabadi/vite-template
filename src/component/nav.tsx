80697309ed1ad9bc62b8cd95ac172a566edb041f
bektidev <bekti@primaku.com>
2023-08-31 20:43:11 +0700

    refc: change navbar color

--- src/component/nav.tsx
+++ src/component/nav.tsx
@@ -2,41 +2,41 @@ import React, { useState } from "react";
 
 function Navbar() {
 
-  const [toogleIcon, setToogleIcon] = useState('fa-solid fa-bars cursor-pointer text-2xl md:hidden')
+  const [toogleIcon, setToogleIcon] = useState('fa-solid fa-bars cursor-pointer text-2xl md:hidden text-slate-200')
 
   const onToggleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    toogleIcon == 'fa-solid fa-bars cursor-pointer text-2xl md:hidden' ? 
-    setToogleIcon('fa-solid fa-xmark cursor-pointer text-2xl md:hidden'):
-    setToogleIcon('fa-solid fa-bars cursor-pointer text-2xl md:hidden')
+    toogleIcon == 'fa-solid fa-bars cursor-pointer text-2xl md:hidden text-slate-200' ? 
+    setToogleIcon('fa-solid fa-xmark cursor-pointer text-2xl md:hidden text-slate-200'):
+    setToogleIcon('fa-solid fa-bars cursor-pointer text-2xl md:hidden text-slate-200')
     const navlink = document.querySelector('.nav-links')
     navlink?.classList.toggle('top-[3%]')
   }
 
   return (
     <>
-      <div className="container-lg bg-slate-200">
+      <div className="container-lg bg-slate-900">
       <nav className="flex justify-between items-center w-[80%] mx-auto">
         <div>
-          <p className="my-5 md:m-8">Logos</p>
+          <p className="my-4 md:m-8 text-lg font-bold text-slate-200 cursor-pointer">Logos</p>
         </div>
-        <div className="nav-links duration-500 md:static absolute md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 my-5 z-10 bg-slate-200">
+        <div className="nav-links duration-500 md:static absolute md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 my-5 z-10 bg-slate-900">
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[3vw] gap-4">
             <li>
-              <a href="#" className="hover:text-gray-500">Home</a>
+              <a href="#" className="text-lg font-bold text-slate-200 hover:text-gray-500 hover:border-b-4 border-slate-500 py-2 px-1">Home</a>
             </li>
             <li>
-              <a href="#" className="hover:text-gray-500">Service</a>
+              <a href="#" className="text-lg font-bold text-slate-200 hover:text-gray-500 hover:border-b-4 border-slate-500 py-2 px-1">Service</a>
             </li>
             <li>
-              <a href="#" className="hover:text-gray-500">About</a>
+              <a href="#" className="text-lg font-bold text-slate-200 hover:text-gray-500 hover:border-b-4 border-slate-500 py-2 px-1">About</a>
             </li>
             <li>
-              <a href="#" className="hover:text-gray-500">Contact</a>
+              <a href="#" className="text-lg font-bold text-slate-200 hover:text-gray-500 hover:border-b-4 border-slate-500 py-2 px-1">Contact</a>
             </li>
           </ul>
         </div>
-        <div className="flex items-center gap-6">
-          <button className="bg-slate-200 px-5 py-2 rounded-full">Sign In</button>
+        <div className="flex items-center gap-6 md:hidden">
+          {/* <button className="bg-slate-200 px-5 py-2 rounded-full">Sign In</button> */}
           <i onClick={onToggleMenu} className={toogleIcon}></i>
         </div>
 
