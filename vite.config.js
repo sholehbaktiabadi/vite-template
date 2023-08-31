dd77848eddec1b722e623f68649d4e1be4399d2b
bektidev <bekti@primaku.com>
2023-08-31 22:33:12 +0700

    chnage port variable from .env file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { configDotenv } from 'dotenv';
+configDotenv()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: +process.env.VITE_APP_PORT
+  }
 })
